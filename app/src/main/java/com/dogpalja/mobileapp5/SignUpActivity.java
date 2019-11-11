72c67c46e9be22df0f088e6400917a22af01aff9
mimseong <parkseongmin98@gmail.com>
2019-11-11 14:16:57 +0900

    작업했던 파일 올림 (로그인, 프로필)

---
+++ app/src/main/java/com/dogpalja/mobileapp5/SignUpActivity.java
@@ -0,0 +1,19 @@
+package com.dogpalja.mobileapp5;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class SignUpActivity extends AppCompatActivity{
+    LinearLayout mLoginContainer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        mLoginContainer = (LinearLayout) findViewById(R.id.login_container);
+
+
+    }
+}
---
+++ app/src/main/java/com/dogpalja/mobileapp5/URLS.java
@@ -0,0 +1,7 @@
+package com.dogpalja.mobileapp5;
+
+public class URLS {
+
+    static final String login_api = "";
+    static final String sign_up_api = "";
+}
